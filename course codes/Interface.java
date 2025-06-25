f1f0d4fed40f732ef31832aff6452e08942da507
NehashaAle <magarni238@gmail.com>
2025-06-25 15:43:44 +0545

    Seventh commit

---
+++ course codes/Interface.java
@@ -0,0 +1,28 @@
+interface InA {
+    int age = 21;
+    String name = "Himal";
+
+    void show();
+
+    void display();
+}
+
+class B implements InA {
+    public void show() {
+        System.out.println("in show");
+    }
+
+    public void display() {
+        System.out.println("in display");
+    }
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+        obj.display();
+
+        System.out.println(InA.age + InA.name);
+    }
+}
