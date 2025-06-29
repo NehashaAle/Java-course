471d02c6b204d603003ddad2abfc81b0db54870a
NehashaAle <magarni238@gmail.com>
2025-06-29 19:02:46 +0545

    Ways to accept input from users.

---
+++ course codes/BufferReaderInput.java
@@ -0,0 +1,25 @@
+// import java.io.BufferedReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BufferReaderInput {
+    public static void main(String arg[]) throws IOException {
+
+        System.out.println("Enter a number");
+        // int num = System.in.read();
+
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader obj = new BufferedReader(in);
+
+        // int num = Integer.parseInt(obj.readLine());
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
