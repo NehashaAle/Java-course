cb4a9a5332987b401556a95613ba84fdf912a564
NehashaAle <magarni238@gmail.com>
2025-07-03 18:30:16 +0545

    Runnable inplace of Thread

---
+++ course codes/RunnableThread.java
@@ -0,0 +1,72 @@
+public class RunnableThread {
+    public static void main(String[] args) {
+        class RunA implements Runnable {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hi");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        class RunB implements Runnable {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        // Runnable
+        Runnable obj1 = new RunA();
+        Runnable obj2 = new RunB();
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        // Using Lamda Expression
+        Runnable o1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hi");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Runnable o2 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Thread trd1 = new Thread(obj1);
+        Thread trd2 = new Thread(obj2);
+
+        trd1.start();
+        trd2.start();
+    }
+}
