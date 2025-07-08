f726baf14f9fc471d92694348df4788e9503e186
NehashaAle <magarni238@gmail.com>
2025-07-08 18:38:46 +0545

    Concept of Var in java

---
+++ course codes/VarExample.java
@@ -0,0 +1,21 @@
+public class VarExample {
+
+    // class var //Invalid
+    // {
+
+    // }
+
+    // var num = 66; // invalid
+
+    // Var can be used as a local variable only
+    public static void main(String[] args) {
+        int i = 9;
+        var j = 6; // valid
+
+        int a;
+        // var b; // invalid
+
+        int var = 3;
+    }
+
+}
