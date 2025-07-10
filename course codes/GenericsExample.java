75b0a005690c38843b1ab121d38a1bb3fd6a9051
NehashaAle <magarni238@gmail.com>
2025-07-10 14:08:19 +0545

    Generics in Java

---
+++ course codes/GenericsExample.java
@@ -0,0 +1,68 @@
+//Generic Class
+class Vegetables<T> {
+    T data;
+
+    void set(T data) {
+        this.data = data;
+    }
+
+    T get() {
+        return data;
+    }
+}
+
+class Dog {
+    // Generic Method
+    public <T> void displayArray(T[] array) {
+        for (T item : array) {
+            System.out.println(item);
+        }
+    }
+}
+
+// Bounded Generic Types
+class Data<T extends Number> {
+    T[] nums;
+
+    Data(T[] nums) {
+        this.nums = nums;
+    }
+
+    float sum() {
+        float sum = 0f;
+        for (T n : nums) {
+            sum += n.floatValue();
+        }
+        return sum;
+    }
+}
+
+public class GenericsExample {
+    public static void main(String[] args) {
+        // Generic Class
+        Vegetables<String> vegies = new Vegetables<>();
+        vegies.set("Onion");
+        System.out.println("Data: " + vegies.get());
+
+        Vegetables<Integer> nums = new Vegetables();
+        nums.set(45);
+        System.out.println("Data: " + nums.get());
+
+        // Generic Method
+        String[] names = { "GermanSeferd", "BullDog", "TibetianMastiff" };
+        Integer[] number = { 45, 70, 9, 66, 41 };
+        Dog obj = new Dog();
+
+        obj.displayArray(names);
+        obj.displayArray(number);
+
+        // Bounded Generic Types
+        Integer[] numb = { 44, 75, 81, 90, 65 };
+        Data<Integer> n = new Data<>(numb);
+        System.out.println("Total int sum = " + n.sum());
+
+        Float[] f1 = { 22f, 6.6f, 81f };
+        Data<Float> f = new Data<>(f1);
+        System.out.println("Total floating sum = " + f.sum());
+    }
+}
